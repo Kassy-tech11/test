94d4d4d93711876455d1aa9f6a3813489677578c
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:06:14 +0200

    Add GhostNet entity with properties and methods

--- ghostnet/src/main/java/de/seashepard/ghostnet/entity/GhostNet.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/entity/GhostNet.java
@@ -1,1 +1,84 @@
 entity/GhostNet.java
+  package de.sheasepherd.ghostnet.entity;
+
+import de.sheasepherd.ghostnet.enums.GhostNetStatus;
+import jakarta.persistence.*;
+
+@Entity
+public class GhostNet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double latitude;
+    private Double longitude;
+    private String estimatedSize;
+
+    private String reporterName;
+    private String reporterPhone;
+    private boolean anonymous;
+
+    @Enumerated(EnumType.STRING)
+    private GhostNetStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getEstimatedSize() {
+        return estimatedSize;
+    }
+
+    public void setEstimatedSize(String estimatedSize) {
+        this.estimatedSize = estimatedSize;
+    }
+
+    public String getReporterName() {
+        return reporterName;
+    }
+
+    public void setReporterName(String reporterName) {
+        this.reporterName = reporterName;
+    }
+
+    public String getReporterPhone() {
+        return reporterPhone;
+    }
+
+    public void setReporterPhone(String reporterPhone) {
+        this.reporterPhone = reporterPhone;
+    }
+
+    public boolean isAnonymous() {
+        return anonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        this.anonymous = anonymous;
+    }
+
+    public GhostNetStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GhostNetStatus status) {
+        this.status = status;
+    }
+}
