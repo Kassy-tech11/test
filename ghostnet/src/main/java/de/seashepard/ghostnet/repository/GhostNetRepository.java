b874c83a838e0b0678f4fea00abb0e08c6be14f1
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:06:49 +0200

    Create GhostNetRepository interface for data access

--- ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
@@ -1,1 +1,8 @@
 repository/GhostNetRepository.java
+package de.sheasepherd.ghostnet.repository;
+
+import de.sheasepherd.ghostnet.entity.GhostNet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GhostNetRepository extends JpaRepository<GhostNet, Long> {
+}
