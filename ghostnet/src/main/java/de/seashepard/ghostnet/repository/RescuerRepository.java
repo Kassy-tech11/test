8286cf7e6d2a45a1b2a03b32c118ec875638d30f
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:19:06 +0200

    Create RescuerRepository interface for JPA

---
+++ ghostnet/src/main/java/de/seashepard/ghostnet/repository/RescuerRepository.java
@@ -0,0 +1,7 @@
+package de.sheasepherd.ghostnet.repository;
+
+import de.sheasepherd.ghostnet.entity.Rescuer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RescuerRepository extends JpaRepository<Rescuer, Long> {
+}
