4829429bde039cdfcc16d24551d78bf04464eb4d
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:19:19 +0200

    Add method to find GhostNet by status

--- ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
@@ -6,3 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GhostNetRepository extends JpaRepository<GhostNet, Long> {
 }
+package de.sheasepherd.ghostnet.repository;
+
+import de.sheasepherd.ghostnet.entity.GhostNet;
+import de.sheasepherd.ghostnet.enums.GhostNetStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GhostNetRepository extends JpaRepository<GhostNet, Long> {
+
+    List<GhostNet> findByStatus(GhostNetStatus status);
+}
