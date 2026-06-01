7359385a30bf46ac69f2b52a3be3c904cdc34ae1
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-01 18:18:32 +0200

    Add rescuer field with getter and setter methods

--- ghostnet/src/main/java/de/seashepard/ghostnet/GhostNet.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/GhostNet.java
@@ -1,1 +1,10 @@
 src/main/java/de/sheasepherd/ghostnet/entity/GhostNet.java
+@ManyToOne
+private Rescuer rescuer;
+public Rescuer getRescuer() {
+    return rescuer;
+}
+
+public void setRescuer(Rescuer rescuer) {
+    this.rescuer = rescuer;
+}
