5b5f9083528c4a05689e7cf7d64af4fc38a517b9
Kassy-tech11 <kassandra.mucks@gmail.com>
2026-06-08 12:05:40 +0200

    Update GhostNetRepository.java

--- ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
+++ ghostnet/src/main/java/de/seashepard/ghostnet/repository/GhostNetRepository.java
@@ -1,15 +1,15 @@
 repository/GhostNetRepository.java
-package de.sheasepherd.ghostnet.repository;
+package de.seashepard.ghostnet.repository;
 
-import de.sheasepherd.ghostnet.entity.GhostNet;
+import de.seashepard.ghostnet.entity.GhostNet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GhostNetRepository extends JpaRepository<GhostNet, Long> {
 }
-package de.sheasepherd.ghostnet.repository;
+package de.seashepard.ghostnet.repository;
 
-import de.sheasepherd.ghostnet.entity.GhostNet;
-import de.sheasepherd.ghostnet.enums.GhostNetStatus;
+import de.seashepard.ghostnet.entity.GhostNet;
+import de.seashepard.ghostnet.enums.GhostNetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
